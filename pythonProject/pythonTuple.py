49eaa78d6f5baa184a570e0ceab0912876804041
izazahmed0177 <izazahmed0177@gmail.com>
2024-07-08 14:42:38 +0600

    python tuple

---
+++ pythonProject/pythonTuple.py
@@ -0,0 +1,22 @@
+
+# tuple
+
+tup=()
+print(type(tup))
+print(tup)
+
+x=(1,2,3,4,"emon",[12,34,56,6.77,"jjhg"],(4,"banana",3.66),1.443,3+4j,222,766)
+print(x)
+print(x[4])
+print(x[5])
+print(x[6])
+print(x[7])
+print(type(x[5]))
+print(type(x[6]))
+print(type(x[8]))
+print(x[5][3])
+print(x[-5][1])
+print(x[4:9])
+
+x1 = (1, 2, 3)
+print(len(x1))
